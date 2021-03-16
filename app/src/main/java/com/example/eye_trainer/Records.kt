fd25ccb25221e930f6d36251fc637216aaf8e352
Roman Shestopalov <shystik-pushystik@mail.ru>
2021-03-16 22:06:58 +0300

    UI and simple task

---
+++ app/src/main/java/com/example/eye_trainer/Records.kt
@@ -0,0 +1,22 @@
+package com.example.eye_trainer
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_records.*
+
+class Records : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_records)
+
+        name.text = intent.getStringExtra("player")
+        score.text = intent.getStringExtra("score")
+
+        button2.setOnClickListener {
+            val menu = Intent(this, MainActivity::class.java)
+            startActivity(menu)
+        }
+
+    }
+}
